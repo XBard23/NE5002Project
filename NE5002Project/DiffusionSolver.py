b92eca5b9276407b93188ce3501012280ff2a9f0
XBard23 <bard.41@osu.edu>
2023-12-06 00:13:52 -0500

    Final copy
    aGrade this one.

--- NE5002Project/DiffusionSolver.py
+++ NE5002Project/DiffusionSolver.py
@@ -12,12 +12,6 @@ Libraries:
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
-
-
-"""
-Timing of methods using decorator functions
-"""
 
 """
 Input data 
@@ -41,19 +35,11 @@ def extractInput(fileInput):
                 'x-end': df.at[1, col],
                 'absorption': df.at[2, col],
                 'scattering': df.at[3, col],
+                'source': df.at[4, col],
             }
             material_count += 1
-            
-    #Data optimization
-    del col
-    del df
-    del material_count
-    del material_key
     
     return materials
-
-
-#Check input data
 
 #Create mesh and show user
 #Mesh size will be generated based on the neutron diffusion length
@@ -67,67 +53,25 @@ def mesh(materials):
         length = xEnd - xStart
         mat['L'] = L
         mat['D'] = D
+        
         # Initialize dxy as half the diffusion length
         dxy = L / 2
 
         # Check if dxy is too large for the material's length
         if dxy > length:
-            dxy = length//2
+            dxy = length/2
 
-        # Adjust dxy to be the largest divisor of total_length less than or equal to L
+        # Adjust dxy to be the largest divisor of the material length less than a % of to L
         while length % dxy > 0.4 * L:
             dxy -= 0.1 * L  # Decrement dxy by 0.1*L until it's a suitable divisor
         
+        #Use the smallest mesh size of all the materials
         if meshSize == 0 or meshSize > dxy:
             meshSize = dxy
         
         xStart = xEnd
 
     return meshSize
-
-
-def plot_mesh_and_materials(meshPoints, materials):
-    # Set up the figure and axis
-    fig, ax = plt.subplots(figsize=(10, 10))
-
-    # Define variables for the total x extent
-    total_xEnd = max(material['x-end'] for material in materials.values())
-
-    # Plot the boundaries and mesh for each material in the first quadrant
-    previous_xEnd = 0
-    for key, material in materials.items():
-        xEnd = material['x-end']
-
-        # Vertical line
-        ax.plot([xEnd, xEnd], [0, xEnd], color='black', linewidth=2)  # Vertical line
-
-        # Horizontal line for material 1
-        if key == 'm1':
-            ax.plot([previous_xEnd, xEnd], [xEnd, xEnd], color='black', linewidth=2)
-
-        previous_xEnd = xEnd
-
-    # Extend the material 1 boundary to form a larger square
-    m1_xEnd = materials['m1']['x-end']
-    ax.plot([-m1_xEnd, m1_xEnd], [m1_xEnd, m1_xEnd], color='black', linewidth=2)  # Top horizontal line
-    ax.plot([-m1_xEnd, -m1_xEnd], [-m1_xEnd, m1_xEnd], color='black', linewidth=2)  # Left vertical line
-    ax.plot([-m1_xEnd, m1_xEnd], [-m1_xEnd, -m1_xEnd], color='black', linewidth=2)  # Bottom horizontal line
-    ax.plot([m1_xEnd, m1_xEnd], [-m1_xEnd, m1_xEnd], color='black', linewidth=2)  # Right vertical line
-
-    # Overlay the mesh points as a lighter grid within the entire area
-    for x in np.arange(-total_xEnd, total_xEnd + meshPoints, meshPoints):
-        ax.axvline(x, color='lightblue', linestyle='--', linewidth=0.5)
-        ax.axhline(x, color='lightblue', linestyle='--', linewidth=0.5)
-
-    ax.set_xlim(-total_xEnd, total_xEnd)
-    ax.set_ylim(-total_xEnd, total_xEnd)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_title('Mesh and Materials Visualization')
-    plt.show()
-
-
-
 
 def plotResults(matrixA, flux, n, dxy, end):
     matrix = np.zeros((n, n))
@@ -150,45 +94,34 @@ def plotResults(matrixA, flux, n, dxy, end):
     ax.set_ylim(ax.get_ylim()[::-1])
     plt.show()
     
-    #X plot
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
-    ax2.plot(x[2, :], matrix[2, :], color='blue')
-    ax2.set_title("Flux along x-axis at y = 0")
-    ax2.set_xlabel("X")
-    ax2.set_ylabel("Flux")
-    plt.show()
     
-    #y=x diagonal
+    
+    # y=x diagonal
     fig3 = plt.figure()
     ax3 = fig3.add_subplot(111)
-    diagonal_values = np.array([matrix[i, i] for i in range(n)])
-    ax3.plot(x[0, :], diagonal_values, color='green')
+    diagonalValues = np.array([matrix[i, i] for i in range(n)])
+    ax3.plot(x[0, :], diagonalValues, color='green')
     ax3.set_title("Flux along y = x")
     ax3.set_xlabel("y = x")
     ax3.set_ylabel("Flux")
+    
+    y_min, y_max = ax3.get_ylim()
+    
+    plt.show()
+    
+    # X plot
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(111)
+    ax2.plot(x[0, :], matrix[0, :], color='blue')
+    ax2.set_title("Flux along x-axis at y = 0")
+    ax2.set_xlabel("X")
+    ax2.set_ylabel("Flux")
+    
+    # Apply the same y-axis limits to fig3
+    ax2.set_ylim([y_min, y_max])
     plt.show()
 
     return
-
-"""
-Version data
-
-"""
-#Create output file
-
-#Metadata information to output file (Call input data)
-
-#Data visualizer
-
-#Output
-
-
-"""
-Input echo
- 
-"""
-#Print input to output
 
 
 """
@@ -204,8 +137,8 @@ def createMatrix(materials, dxy):
     D2 = materials['m2']['D']
     a1 = materials['m1']['absorption']
     a2 = materials['m2']['absorption']
-    S1 = 5
-    S2 = 5
+    S1 = materials['m1']['source']
+    S2 = materials['m2']['source']
     
     n = int((m2End + 2*D2)//dxy)
     x1 = int(m1End//dxy)
@@ -215,7 +148,8 @@ def createMatrix(materials, dxy):
     sourceVector = []
     sv = []
     
-    #Create variables, refer to README.txt for clarification of numbering
+    #Create variables, LR= Left reflective, FS = Free Space, RR = right diagonol reflective
+    #Int = interface
     dEqs = {
         '0': descretizedEqs(0, 0, 0, D1, 0, dxy, 0, dxy, 0, S1, 0, a1), #Corner
         '1': descretizedEqs(0, D1, 0, D1, 0, dxy, dxy, dxy, S1, S1, a1, a1), #M1 LR
@@ -229,24 +163,23 @@ def createMatrix(materials, dxy):
         '9': descretizedEqs(D2, 0, D2, D2, dxy, dxy, dxy, dxy, S2, S2, a2, a2), # M2 RR
         }
     
-    #adjustments: 
-        #3, 6, 9( need 1/2 1 and 3 and v2 = 0)
         
     #Build m2End
-    matrix2, sv = buildLine2(dEqs['7'], dEqs['8'], dEqs['9'], n, n, 0)
-    sourceVector = np.append(sourceVector, sv)
+    matrix2, sv = buildLine(dEqs['7'], dEqs['8'], dEqs['9'], n, n, 0)
     matrixA = np.array(matrix2)
     matrixA, sourceVector = iterate(matrixA, matrix2, sourceVector, sv, x2-2, n)
+    sourceVector = np.append(sourceVector,sv)
     #Build interface
-    matrixI, sv = buildLine2(dEqs['4'], dEqs['5'], dEqs['6'], n, x1+1, (x2-1)*n+x2-1)
+    matrixI, sv = buildLine(dEqs['4'], dEqs['5'], dEqs['6'], n, x1+1, (x2-1)*n+x2-1)
     matrixA = np.vstack([matrixI, matrixA])
     sourceVector = np.append(sv, sourceVector)
     #build m1End
     if x1 > 1:
-        matrix1, sv = buildLine2(dEqs['1'], dEqs['2'], dEqs['3'], n, x1, x2*(n+1))
+        matrix1, sv = buildLine(dEqs['1'], dEqs['2'], dEqs['3'], n, x1, x2*(n+1))
         matrixA = np.vstack([matrix1, matrixA])
-        matrixA, sourceVector = iterate(matrixA, matrix1, sourceVector, sv, x1-2, n)
         sourceVector = np.append(sv, sourceVector)
+        matrixA, sourceVector = iterate(matrixA, matrix1, sourceVector, sv, x1-2, n)
+        
     
     #assign corner
     matrixC = [0]*(n*(n+1))
@@ -255,9 +188,9 @@ def createMatrix(materials, dxy):
     sourceVector = np.append(dEqs['0'][6], sourceVector)
 
     #Condense matrix to square
-    nonZero = ~np.all(matrixA == 0, axis = 0) 
-    matrixA = matrixA[:, nonZero]
-    matrixA = matrixA[:, :-n] 
+    nonZero = ~np.all(matrixA == 0, axis = 0) #Get non zero columns
+    matrixA = matrixA[:, nonZero] #Removezero columns
+    matrixA = matrixA[:, :-n] #Extrapoloation adjustment where flux = 0
 
     return matrixA, sourceVector, n
 
@@ -270,7 +203,6 @@ def descretizedEqs(D00, D10, D01, D11, X0, X1, Y0, Y1, S1, S2, E1, E2):
               ]
         
     V = [0.25 * x * y for x in [X0, X1] for y in [Y0, Y1]]
-    aC = 0
     
     #Right Reflective adjustment
     if D10 == 0 and D00 > 0:
@@ -287,14 +219,14 @@ def descretizedEqs(D00, D10, D01, D11, X0, X1, Y0, Y1, S1, S2, E1, E2):
     #Calculate aC and source
     E = V[0]*E1 + V[1]*E2 + V[2]*E1 + V[3]*E2
     S = V[0]*S1 + V[1]*S2 + V[2]*S1 + V[3]*S2
-    aC += E - sum(result)
+    aC = E - sum(result)
     result.extend([aC, E, S])
     
     return result
 
 
 #Build Matrix
-def buildLine2(t, m, b, n, x, adj):
+def buildLine(t, m, b, n, x, adj):
     
     y = 2*n+1
     n2 = n*(n+1)
@@ -332,12 +264,12 @@ def buildLine2(t, m, b, n, x, adj):
 def iterate(matrixA, matrix, sourceVector, sv, stop, n):
     while stop >= 1:
         # Main Matrix adjustments
-        matrix = np.delete(matrix, -2, axis=0)
-        last = np.roll(matrix[-1], -1)
-        matrix[-1] = last
-        matrix = np.hstack([matrix[:, n:], np.zeros((matrix.shape[0], n))])
+        matrix = np.delete(matrix, -2, axis=0) #Delete second to last column
+        last = np.roll(matrix[-1], -1) #Move over last column to allign diagnol
+        matrix[-1] = last #Add back to matrix
+        matrix = np.hstack([matrix[:, n:], np.zeros((matrix.shape[0], n))]) #Shift whole matrix by n
 
-        matrixA = np.vstack([matrix, matrixA])
+        matrixA = np.vstack([matrix, matrixA]) #Add to main
 
         # Source Vector adjustments
         sv = np.delete(sv, 1)
@@ -386,7 +318,6 @@ def diffusionLength(ab, sc):
     L = np.sqrt(D/ab)
     
     return L, D
-
 
 """
 Main
